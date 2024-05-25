ecbbd4f7af3792a3ce5a88c70053161e0c2c6eba
majdyousof <117210724+majdyousof@users.noreply.github.com>
2024-05-26 00:35:38 +0100

    trying pic

--- Home.py
+++ Home.py
@@ -5,15 +5,7 @@ def main():
     st.title("Welcome to The Arrow!")
 
     st.sidebar.success("Select page above.")
-    st.sidebar.title("Welcome!")
-    #st.sidebar.image('\images\logo.jpg', use_column_width='always')
-
-    # Add components to the sidebar
-    st.sidebar.header("Options")
-    option = st.sidebar.selectbox("Select an assessment method", ["Qualitative", "Quantitative"])
-
-    # Add components to the main content area
-    st.write(f"You selected: {option}")
+    st.sidebar.image('\images\logo.jpg', use_column_width='always')
 
 if __name__ == "__main__":
     main()
