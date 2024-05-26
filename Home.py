942a5c35e4cc01ee8f65ed422b41f749813cbb90
majdyousof <117210724+majdyousof@users.noreply.github.com>
2024-05-26 16:31:16 +0100

    construction message

--- Home.py
+++ Home.py
@@ -2,8 +2,7 @@ import streamlit as st
 
 def main():
     # Set the title and layout of the dashboard
-    st.title("Welcome to The Arrow!")
-
+    st.title('Under construction :hammer:')
     st.sidebar.success("Select page above.")
     st.sidebar.image('images/logo.jpg', use_column_width='always')
 
