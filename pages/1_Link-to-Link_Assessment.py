cd2cb3ac9ab90f3268aa1edf4c6fea047430de6a
majdyousof <117210724+majdyousof@users.noreply.github.com>
2024-06-03 19:53:47 +0100

    Revert "this is it"
    This reverts commit 7819bef9ceba2c033452e55447abec3eef920133.

--- pages/1_Link-to-Link_Assessment.py
+++ pages/1_Link-to-Link_Assessment.py
@@ -237,7 +237,7 @@ barplot = go.Figure(data=[go.Bar(name = 'Rail solution',
                                   orientation='h',
                                   marker = dict(color = 'darkorange'))])
 
-barplot.update_layout(title = 'Average Qualitative Assessment Scores per Link', xaxis_range = [1,4])
+barplot.update_layout(title = 'Average Qualitative Assessment Scores per Link')
 
 st.plotly_chart(barplot)
 
@@ -252,6 +252,6 @@ barplot2 = go.Figure(data=[go.Bar(name = 'Rail solution',
                                   orientation='h',
                                   marker = dict(color = 'darkorange'))])
 
-barplot2.update_layout(title = 'Average Qualitative Assessment Scores per Social and Feasibility Factor', xaxis_range = [1,4])
+barplot2.update_layout(title = 'Average Qualitative Assessment Scores per Social and Feasibility Factor')
 
 st.plotly_chart(barplot2)
